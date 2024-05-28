51e09776e32b6a8951e84b30a1ebce37e646feeb
999trojan <160748929+999trojan@users.noreply.github.com>
2024-05-28 11:01:12 +0300

    Update config.js

--- config.js
+++ config.js
@@ -100,7 +100,7 @@ module.exports = {
 
 
 
-global.ELEVENLAB_API_KEY = process.env.ELEVENLAB_API_KEY || "";
+global.ELEVENLAB_API_KEY = process.env.ELEVENLAB_API_KEY || "e69ec67952fbaadaa1ddb831b3817c2d";
 global.aitts_Voice_Id = process.env.AITTS_ID|| "37";
 
 
